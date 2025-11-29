46e19322f90be3840b88a7c5bc2d0844a4220dd7
Gavriel-sys <gilbertdvs19@gmail.com>
2025-11-30 00:50:20 +0700

    terlambat update challenge 5

--- src/challenge1.ts
+++ src/challenge1.ts
@@ -1,2 +1,44 @@
+type Book = {
+  title: string;
+  author: string;
+  publicationYear: number;
+};
+
+const books: Book[] = [];
+
+function addBook(book: Book): void {
+  books.push(book);
+}
+
+function listBooks(): void {
+  books.forEach((book) => {
+    console.log(`${book.title} by ${book.author} (${book.publicationYear})`);
+  });
+}
+
+function searchBook(title?: string): void {
+  if (title !== undefined) {
+    console.log(`Search Result for "${title}":`);
+    books.forEach((book) => {
+      if (book.title === title) {
+        console.log(
+          `${book.title} by ${book.author} (${book.publicationYear})`
+        );
+      }
+    });
+  }
+}
+
+addBook({
+  title: `The Great Gatsby`,
+  author: `F. Scott Fitzgerald`,
+  publicationYear: 1925,
+});
+addBook({ title: `1984`, author: `George Orwell`, publicationYear: 1949 });
+
+listBooks();
+
+searchBook(`1984`);
+
 // Don't delete code bellow and this code must be at the bottom of the file
 export { addBook, listBooks, searchBook };
